3ca971593a9ef4a53b8e9093be9ab428b088377d
kunic <kunic@DESKTOP-3DUV7TD>
2023-02-21 23:36:19 +0700

    Добавьте файлы проекта.

---
+++ Массивы/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("С какого числа начнем цикл");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Сколько циклов?");
+int b = int.Parse(Console.ReadLine());
+int[] NewArray = Enumerable.Range(n,b).ToArray();
+for (int i = 0;i < n; i++)   
+{
+    Console.WriteLine(NewArray[i]);
+    
+}
+    
+//Console.WriteLine(NewArray);
+Console.ReadLine();
